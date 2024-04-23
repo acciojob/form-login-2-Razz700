bc9eb3b9bf5630a3f671d322fe7a115b36471ed9
Razz700 <160166041+Razz700@users.noreply.github.com>
2024-04-23 15:45:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+let fn=document.getElementById("firstname");
+let ls=document.getElementById("lastname");
+let ph=document.getElementById("phone");
+let em=document.getElementById("email");
+let sub=document.getElementById("subm");
+sub.addEventListener("click",myfnc);
+function myfnc(){
+alert(`First Name: ${fn.value} Last Name: ${ls.value} Phone Number: ${ph.value} Email ID: ${em.value}`);
+}
